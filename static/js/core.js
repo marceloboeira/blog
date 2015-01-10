b7081b3cd5166085c626e904a8842f02f1c418da
Marcelo Boeira <me@marceloboeira.com>
2015-01-10 11:31:00 -0200

    Some refactoring and warning removals...

--- static/js/core.js
+++ static/js/core.js
@@ -1,11 +1,14 @@
+/**
+ * Basic stuff
+ * @param  {[type]}
+ * @return {[type]}
+ */
 $(function(){
-
 	var toggleSidebar = $("#togglesidebar");
 	var primary = $("#primary");
 	var secondary = $("#secondary");
 
 	toggleSidebar.on("click", function(){
-
 		if(primary.hasClass("col-sm-9")){
 			primary.removeClass("col-sm-9");
 			primary.addClass("col-sm-12");
@@ -17,5 +20,4 @@ $(function(){
 			secondary.css('display', 'inline-block');
 		}
 	});
-
 });
