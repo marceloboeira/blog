f6768ebadae7ca11d6cdd4cf41465bb603a20d57
Marcelo Boeira <me@marceloboeira.com>
2015-06-11 18:03:45 -0300

    Plugins removal

--- static/js/core.js
+++ static/js/core.js
@@ -4,20 +4,20 @@
  * @return {[type]}
  */
 $(function(){
-	var toggleSidebar = $("#togglesidebar");
-	var primary = $("#primary");
-	var secondary = $("#secondary");
+  var toggleSidebar = $("#togglesidebar");
+  var primary = $("#primary");
+  var secondary = $("#secondary");
 
-	toggleSidebar.on("click", function(){
-		if(primary.hasClass("col-sm-9")){
-			primary.removeClass("col-sm-9");
-			primary.addClass("col-sm-12");
-			secondary.css('display', 'none');
-		}
-		else {
-			primary.removeClass("col-sm-12");
-			primary.addClass("col-sm-9");
-			secondary.css('display', 'inline-block');
-		}
-	});
+  toggleSidebar.on("click", function(){
+    if(primary.hasClass("col-sm-9")){
+      primary.removeClass("col-sm-9");
+      primary.addClass("col-sm-12");
+      secondary.css('display', 'none');
+    }
+    else {
+      primary.removeClass("col-sm-12");
+      primary.addClass("col-sm-9");
+      secondary.css('display', 'inline-block');
+    }
+  });
 });
